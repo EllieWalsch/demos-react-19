c7435250ed14e220cf9d6f719b83dba34af2ebd9
EllieWalsch <elliewalsch4@gmail.com>
2023-03-02 21:24:40 -0600

    Render one cat

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
+import CatCard from "./components/CatCard";
+import catsData from "./data/cats";
 
 function App() {
   // text is a prop passed from parent (App) to child (Header)
-  return <Header text="Hello World" />;
+  return <CatCard cat={catsData[0]} />;
 }
 
 export default App;
