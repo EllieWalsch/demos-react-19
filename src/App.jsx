d72421043e3bdd35b9dfa429594947956f206c26
EllieWalsch <elliewalsch4@gmail.com>
2023-03-06 20:34:30 -0600

    Style navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import catsData from "./data/cats";
 import groceriesData from "./data/groceries";
 import UserCard from "./components/UserCard";
 import userData from "./data/users";
+import NavBar from "./components/NavBar";
 
 function App() {
   // text is a prop passed from parent (App) to child (Header)
   // <> is a fragment tag, use that instead of divs
   return (
     <>
+      <NavBar />
+
       <Header text={"Hello Cat!"} />
 
       {catsData.map((cat) => {
